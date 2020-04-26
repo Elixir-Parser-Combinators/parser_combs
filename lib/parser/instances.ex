2fe0f7a1068e533e71d7bd5d14e88753646f80c8
Guenther Schmidt <schmig68@gmail.com>
2020-04-26 00:31:17 -0500

    typeclass instances for parser, incl. test

---
+++ lib/parser/instances.ex
@@ -0,0 +1,60 @@
+defmodule Parser.Instances do
+  @moduledoc false
+
+  import Typeclasses.Monad
+
+  @success :success
+  @failure :failure
+
+  inject_fmap_pure_apply_empty_choice_bind(
+    &_fmap/2,
+    &_pure/1,
+    &_apply/2,
+    fn _ -> @failure end,
+    &_choice/2,
+    &_bind/2
+  )
+
+  defp _fmap(a2b, fa) do
+    fn input ->
+      case fa.(input) do
+        @failure -> @failure
+        {@success, value, remainder} -> {@success, a2b.(value), remainder}
+      end
+    end
+  end
+
+  defp _pure(value) do
+    fn input -> {@success, value, input} end
+  end
+
+  defp _apply(pa2b, pa) do
+    fn input ->
+      case pa2b.(input) do
+        @failure ->
+          @failure
+
+        {@success, a2b, remainder} ->
+          fmap(a2b, pa).(remainder)
+      end
+    end
+  end
+
+  defp _choice(pa1, pa2) do
+    fn input ->
+      case pa1.(input) do
+        @failure -> pa2.(input)
+        {@success, _value, _remainder} = success -> success
+      end
+    end
+  end
+
+  defp _bind(pa, a2pb) do
+    fn input ->
+      case pa.(input) do
+        @failure -> @failure
+        {@success, value, remainder} -> a2pb.(value).(remainder)
+      end
+    end
+  end
+end
--- test/parser_test.exs
+++ test/parser_test.exs
@@ -1,8 +1,52 @@
 defmodule ParserTest do
   use ExUnit.Case
-  doctest Parser
+  use ExUnitProperties
 
-  test "greets the world" do
-    assert Parser.hello() == :world
+  import Parser.Instances
+
+  def curry3(fun) do
+    fn a -> fn b -> fn c -> fun.(a, b, c) end end end
+  end
+
+  describe "testing typeclass laws" do
+    property "testing functor laws, fmap" do
+      check all(input <- binary(), n <- integer()) do
+        for fun <- [fn x -> x end, fn x -> x + 1 end] do
+          assert fmap(fun, pure(n)).(input) == pure(fun.(n)).(input)
+        end
+      end
+    end
+
+    property "testing applicative laws, pure and <~>" do
+      check all(input <- binary(), a <- integer(), b <- integer(), c <- integer()) do
+        for fun <- [fn a, b, c -> a + b + c end] do
+          assert (pure(curry3(fun)) <~> pure(a) <~> pure(b) <~> pure(c)).(input) ==
+                   pure(fun.(a, b, c)).(input)
+        end
+      end
+    end
+
+    property "testing alternative laws" do
+      check all(input <- binary(), n <- integer()) do
+        assert (pure(n) <|> empty()).(input) == pure(n).(input)
+        assert (empty() <|> pure(n)).(input) == pure(n).(input)
+      end
+    end
+
+    property "testing monad laws" do
+      check all(input <- binary(), a <- integer(), b <- integer()) do
+        for f <- [fn x -> return(a + x) end, fn x -> return(a * x * 5) end],
+            g <- [fn x -> return(a * x - 5) end, fn x -> return(a * 14 - x) end] do
+          assert (return(b) ~>> f).(input) ==
+                   f.(b).(input)
+
+          assert (return(b) ~>> (&return/1)).(input) ==
+                   return(b).(input)
+
+          assert (return(b) ~>> f ~>> g).(input) ==
+                   (return(b) ~>> fn x -> f.(x) ~>> g end).(input)
+        end
+      end
+    end
   end
 end
