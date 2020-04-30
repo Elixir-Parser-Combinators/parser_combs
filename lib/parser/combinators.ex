3bf71f7b03ae26ca112ac857e7beab92aa900738
Guenther Schmidt <schmig68@gmail.com>
2020-04-30 02:58:27 -0500

    refactored code

--- lib/parser/combinators.ex
+++ lib/parser/combinators.ex
@@ -1,44 +1,21 @@
 defmodule Parser.Combinators do
   @moduledoc false
 
-  use Parser.Instances
+  use Parser.Continuation
 
-  defmacro __using__(_options) do
-    quote do
-      import unquote(__MODULE__)
-    end
-  end
-
-  def parse(parser, input) do
-    parser.(input)
-  end
-
-  def some(parser) do
-    many(parser) <|> return([])
-  end
-
-  def many(parser) do
+  def satisfy(fun?) do
     monad do
-      x <- parser
-      xs <- some(parser)
-      return([x | xs])
-    end
-  end
+      x <- ic(elem())
 
-  # TODO find more elegant solution
-  def try_parse(parser) do
-    fn input ->
-      case parser.(input) do
-        {:success, value, _remainder} -> return(value).(input)
-        failure -> failure
+      if(fun?.(x)) do
+        return(x)
+      else
+        ic(empty())
       end
     end
   end
 
-  # TODO find more elegant solution
-  def many_till(parser, end_parser) do
-    end_parser
-    ~>> fn _ -> return([]) end
-    <|> (parser ~>> fn x -> many_till(parser, end_parser) ~>> fn xs -> return([x | xs]) end end)
+  def char(c) do
+    satisfy(fn x -> x == c end)
   end
 end
---
+++ lib/parser/continuation.ex
@@ -0,0 +1,47 @@
+defmodule Parser.Continuation do
+  @moduledoc false
+
+  use Control.Continuation
+
+  defmacro __using__(_options) do
+    quote do
+      import unquote(__MODULE__)
+      use Control.Continuation
+    end
+  end
+
+  @success :success
+  @failure :failure
+
+  defp _return(a) do
+    fn input -> {@success, a, input} end
+  end
+
+  defp _bind(ma, a2mb) do
+    fn input ->
+      case ma.(input) do
+        {@success, a, remainder} -> a2mb.(a).(remainder)
+        @failure -> @failure
+      end
+    end
+  end
+
+  def elem() do
+    fn
+      <<char::utf8, remainder::binary>> -> {@success, char, remainder}
+      _ -> @failure
+    end
+  end
+
+  def empty() do
+    fn _ -> @failure end
+  end
+
+  def ic(parser) do
+    make_ic(&_bind/2).(parser)
+  end
+
+  def run(m) do
+    make_run(&_return/1).(m)
+  end
+end
